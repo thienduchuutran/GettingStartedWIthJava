8fd3d8319d1a7ce848582e5dd1d3cd9c9eed88f7
Duc <thienductranhuu2784@gmail.com>
2024-12-03 16:46:39 -0500

    init

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package javaCore;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hi java duc");
+		System.out.println("hi java duc");
+		System.out.println("hi java duc");
+		System.out.println("hi java duc");
+	}
+}
